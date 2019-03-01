3af09c6e742b316d47f1d3611965f1628907ff79
cwllight8 <cwllight8@gmail.com>
2019-03-01 10:00:56 -0700

    simple changes

--- java-rpg/src/main/java/edu/neumont/light/javarpg/models/Chest.java
+++ java-rpg/src/main/java/edu/neumont/light/javarpg/models/Chest.java
@@ -1,10 +1,19 @@
 package edu.neumont.light.javarpg.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * @author cole
  *
  */
 public class Chest {
-
+	
+	private Random rand = new Random();
+	
+	private List<Item> loot = new ArrayList<>();
+	
+	public Chest() {}
 	
 }
