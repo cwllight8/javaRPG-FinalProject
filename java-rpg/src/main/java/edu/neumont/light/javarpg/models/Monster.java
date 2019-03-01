ca836c5ebfd761151daf64eaa10ecf2fc72046d9
cwllight8 <cwllight8@gmail.com>
2019-03-01 11:00:14 -0700

    started monster class

--- java-rpg/src/main/java/edu/neumont/light/javarpg/models/Monster.java
+++ java-rpg/src/main/java/edu/neumont/light/javarpg/models/Monster.java
@@ -1,5 +1,25 @@
 package edu.neumont.light.javarpg.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import edu.neumont.light.javarpg.models.enums.MonsterType;
+
 public class Monster {
+	
+	private int level;
+	
+	private int HP;
+	
+	private MonsterType type;
+	
+	private List<Item> loot = new ArrayList<>();
+	
+	private List<Skill> skills = new ArrayList<>();
+	
+	private int basicAttack;
+	
+	private Random rand = new Random();
 
 }
