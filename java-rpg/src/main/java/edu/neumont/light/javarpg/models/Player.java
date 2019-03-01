6d7a023a35eae091683f85681e0eca714d31a755
Lemuel Bonite <ljbonite21@gmail.com>
2019-03-01 09:44:53 -0700

    player

--- java-rpg/src/main/java/edu/neumont/light/javarpg/models/Player.java
+++ java-rpg/src/main/java/edu/neumont/light/javarpg/models/Player.java
@@ -1,8 +1,22 @@
 package edu.neumont.light.javarpg.models;
 
+import java.util.ArrayList;
+
 public class Player {
 
+	private int level;
 	
+	private int xp;
+	
+	private int skillPoints;
+	
+	private int hp;
+	
+	private ArrayList<Item> inventory;
+	
+	private ArrayList<Skill> skills;
+	
+	private Weapon equippedWeapon;
 	
 	
 	
