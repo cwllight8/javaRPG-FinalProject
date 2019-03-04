667ba909d947e2e83b4ced7839114f0372fe84bd
Lemuel Bonite <ljbonite21@gmail.com>
2019-03-04 10:41:10 -0700

    JavaFx init

--- java-rpg/src/main/java/edu/neumont/light/javarpg/JavaRPG.java
+++ java-rpg/src/main/java/edu/neumont/light/javarpg/JavaRPG.java
@@ -1,6 +1,12 @@
 package edu.neumont.light.javarpg;
 
+import java.net.URL;
+
+import edu.neumont.light.javarpg.view.RpgView;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class JavaRPG extends Application {
@@ -10,8 +16,14 @@ public class JavaRPG extends Application {
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
+	public void start(Stage stage) throws Exception {
+		URL location = this.getClass().getClassLoader().getResource("RpgView.fxml");
+		FXMLLoader loader = new FXMLLoader(location);
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		RpgView viewController = loader.getController();
+//		viewController.setStage(stage);
 		
 	}
 
